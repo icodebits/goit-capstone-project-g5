dde263de6e74ee21f9a59b3fca55fe11f83053e4
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-10-29 15:18:32 +0100

    add types for CI

---
+++ src/types/swagger-ui-react.d.ts
@@ -0,0 +1,12 @@
+declare module "swagger-ui-react" {
+  import * as React from "react";
+  const SwaggerUI: React.ComponentType<any>;
+  export default SwaggerUI;
+}
+
+declare module "swagger-ui-react/swagger-ui.css" {
+  const content: string;
+  export default content;
+}
+
+
